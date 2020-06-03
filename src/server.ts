2104e9cd29056796fbf51f7e1e0ea286cbaff1f9
bianca <bianca@buscaprev.com.br>
2020-06-03 18:31:33 -0300

    added first steps - get, post, filter

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,52 @@
-import express from 'express';
+import express, { request } from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {    
-    response.json(
-        [
-            'Marcela',
-            'Ursula',
-            'Roberta',
-            'Marta',
-            'teste',
-        ]
-    );
+// Rota: endereço completo da requisição
+// Recurso: qual entidade estamos acessando do sistema
+
+app.use(express.json());
+
+const users = [
+    'Marcela',
+    'Ursula',
+    'Roberta',
+    'Marta',
+    'teste',
+]
+
+
+app.get('/users', (request, response) => {   
+    const search = String(request.query.search);
+    // search params: parametros geralmente opcionais - usados pra filtros, paginação por ex
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+    
+    return response.json(filteredUsers);
+});
+
+
+app.get('/users/:id',(request, response) =>{
+    const id = Number(request.params.id);
+    // request.params: parametros que vem na rota pra identificar recursos
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+
+app.post('/users', (request, response) => {    
+    const data = request.body;
+    // request body: parametro para criação e atualização de informações
+    console.log(data);
+
+    const user = {
+        name: data.name,
+        email:  data.email
+    }
+
+    return response.json(user);
 });
 
 app.listen(3333);
